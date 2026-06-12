ca837409eeea9210aa8bebb8e5355e3c08f19590
agus-ops <rovanzajunior@gmail.com>
2026-06-12 12:05:19 +0700

    Support Vercel deployment base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/giwa-flowlab/",
+  base: process.env.VERCEL ? "/" : "/giwa-flowlab/",
 });
